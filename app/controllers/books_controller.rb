5a274ccad3c7155f0a6569de6dfb07524e2da4e5
EC2 Default User <ec2-user@ip-172-31-85-128.ec2.internal>
2021-07-24 13:10:47 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
+    @book =Book.new
 
   end
 
@@ -9,20 +12,44 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfuly created.'
+      redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
+
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfuly update"
+    redirect_to book_path
+    else
+    render :edit
+    end
+  end  
 
   private
   def book_params
-    params.require(:list).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
-
-
-  def edit
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   get 'top' => 'homes#top'
-  post 'books' => 'books#index'
 
 
 end
