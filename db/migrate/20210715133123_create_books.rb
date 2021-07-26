0a45ac267dbae7ef52f5036026835dcccd4414b9
EC2 Default User <ec2-user@ip-172-31-85-128.ec2.internal>
2021-07-26 03:31:31 +0000

    Initial Commit

--- db/migrate/20210715133123_create_books.rb
+++ db/migrate/20210715133123_create_books.rb
@@ -3,8 +3,6 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :title
       t.string :body
-      t.validates :title, presence: true
-      t.validates :body, presence: true
 
       t.timestamps
     end
